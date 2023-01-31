c7f27d912d388df55cd6b26129b20a4ca9e9e163
Ivan Semin <ivan.semin@foam.agency>
2023-01-31 18:58:05 +0100

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "./",
+  build: {
+    outDir: './build'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
