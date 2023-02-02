135a93c6ca53a5ed3b6c47eed878716e6dcf40e5
Ivan Semin <ivan.semin@foam.agency>
2023-02-02 21:24:33 +0100

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./CNAME",
+          dest: "./",
+        },
+        {
+          src: "./src/assets/fonts",
+          dest: "./assets",
+        },
+      ],
+    }),
+  ],
   base: "/",
   resolve: {
     alias: {
