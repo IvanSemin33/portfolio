41f5197ac5ba250769e0ba45e4fa188a2c37daca
Ivan Semin <ivanswork24@gmail.com>
2024-09-30 13:01:42 +0200

    Add loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,10 +5,11 @@ import {
   // useWindowSize
 } from "react-use";
 import { useEffect, useRef, useState } from "react";
-import { useLottie } from "lottie-react";
+
 import rotateAnimation from "../assets/rotate.json";
 import loadingAnimation from "../assets/loading.json";
 import useDeviceDetect from "@/hooks/useDeviceDetect";
+import Lottie from "lottie-react";
 
 type SplineProps = React.ComponentProps<typeof Spline>;
 
@@ -16,66 +17,87 @@ type Application = import("@splinetool/runtime").Application;
 
 function App() {
   // const { width, height } = useWindowSize();
-  const { type } = useOrientation();
+  const { type: orientation } = useOrientation();
   const { isMobile } = useDeviceDetect();
 
   const [isLoading, setIsLoading] = useState(true);
   const [shouldRotate, setShouldRotate] = useState(false);
 
+  const [hideLoading, setHideLoading] = useState(isMobile ? true : false);
+  const [hideRotate, setHideRotate] = useState(isMobile ? false : true);
+
   const spline = useRef<Application>();
 
   useEffect(() => {
-    console.log({isMobile, type})
     if (isMobile) {
-      if (type.includes("portrait")) {
+      if (orientation.includes("portrait")) {
         setShouldRotate(true);
+        setHideRotate(false);
       } else {
         setShouldRotate(false);
+        setTimeout(() => {
+          setHideRotate(true);
+        }, 1000);
       }
     }
-  }, [isMobile, type]);
-
-  const { View: RotateAnimation } = useLottie({
-    animationData: rotateAnimation,
-    loop: true,
-    height: 300,
-    width: 300,
-  });
-
-  const { View: LoadingAnimation } = useLottie({
-    animationData: loadingAnimation,
-    loop: true,
-    width: 300,
-    height: 300,
-  });
+  }, [isMobile, orientation]);
 
   const onLoad: SplineProps["onLoad"] = (splineApp) => {
     spline.current = splineApp;
 
     setTimeout(() => {
       setIsLoading(false);
+
+      setTimeout(() => {
+        setHideLoading(true);
+      }, 1000);
     }, 1000);
   };
 
   return (
     <div className={styles.container}>
-      {shouldRotate ? (
-        <div className={styles.animationContainer}>{RotateAnimation}</div>
-      ) : (
-        <>
-          {isLoading && (
-            <div className={styles.animationContainer}>{LoadingAnimation}</div>
-          )}
-          <Spline
-            scene="https://prod.spline.design/x5ftb8OdNCYtzr2y/scene.splinecode"
-            onLoad={onLoad}
-            style={{
-              opacity: isLoading ? 0 : 1,
-              transition: "opacity 1.5s",
-            }}
-          />
-        </>
-      )}
+      <div
+        className={styles.animationContainer}
+        style={{
+          opacity: shouldRotate ? 1 : 0,
+          transition: "opacity 1s",
+          display: hideRotate ? "none" : "flex",
+        }}
+      >
+        <Lottie
+          animationData={rotateAnimation}
+          loop={true}
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            aspectRatio: "1/1",
+          }}
+        />
+      </div>
+
+      <div
+        className={styles.animationContainer}
+        style={{
+          opacity: isLoading ? 1 : 0,
+          transition: "opacity 1s",
+          display: hideLoading ? "none" : "flex",
+        }}
+      >
+        <Lottie
+          animationData={loadingAnimation}
+          loop={true}
+          width={300}
+          height={300}
+        />
+      </div>
+
+      <Spline
+        scene="https://prod.spline.design/x5ftb8OdNCYtzr2y/scene.splinecode"
+        onLoad={onLoad}
+        style={{
+          display: isLoading || shouldRotate ? "none" : "block",
+        }}
+      />
     </div>
   );
 }
